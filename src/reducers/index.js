b1fb28dc2715dfeaf24cd98674f3fea7badb3f7b
baptiste-boimard <bouketin27@gmail.com>
2023-09-01 17:36:10 +0200

    Effacement des éléments de coder le jeu en composant react

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 
 // ==-- IMPORT SLICE--==
-import boardSlice from '../slice/board';
 import utilitiesSlice from '../slice/utilities';
 
 
 const rootReducer = combineReducers({
-  boardReducer: boardSlice,
   utilitiesReducer: utilitiesSlice,
 });
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import rootReducer from '../reducers';
-// import middlewareCompose from '../middlewares';
 
 const store = configureStore({
   reducer: rootReducer,
