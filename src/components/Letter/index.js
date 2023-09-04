118a8e9a29a5a457d74a01cfb32ad74d104d5920
baptiste-boimard <bouketin27@gmail.com>
2023-09-04 15:04:13 +0200

    Composant Letter terminé

--- src/components/Letter/index.js
+++ src/components/Letter/index.js
@@ -28,16 +28,29 @@ function Letter() {
   return (
 
     <Modal show={isOpenLetter}>
-        <Modal.Header closeButton>
-          <Modal.Title>Letter</Modal.Title>
+        <Modal.Header closeButton onHide={handleClose}>
+          <Modal.Title>Ma lettre de motivation</Modal.Title>
         </Modal.Header>
-        <Modal.Body>C'est la Modal Letter</Modal.Body>
+        <Modal.Body>
+          <p>Madame, Monsieur, </p>
+          <br /><br />
+          Tout fraîchement reconverti depuis l'année dernière dans le développement web, je suis à la recherche d'un employeur afin de poursuivre ma formation au sein d'une 3ème année de bachelor pour devenir chef de projets digitaux qui se déroule en alternance. Cet enseignement est dispensé par la Normandie Web School de Rouen et vient compléter ma formation initiale de développeur fullstack javascript dispensée par l'école O'Clock en 2022.
+          <br /><br />
+          Cette année est pour moi une année charnière me permettant de compléter mes connaissances théoriques du métier mais surtout de pouvoir mettre à profit les connaissances acquises lors de ma formation et durant mes projets personnels. 
+          <br /><br />
+          Collaborer avec votre entreprise durant cette année me permettra d'engranger des connaissances et une expérience importante pour la suite de mon projet. Cela vous permettra également de pouvoir travailler avec un collaborateur qui a une expérience professionnelle solide. En effet j'ai pu acquérir grâce à mon expérience passée des compétences et une éthique de travail me permettant de répondre avec efficacité, motivation et méthode aux problématiques et défis que je pourrai rencontrer au cours de cette année d'alternance. 
+          <br /><br />
+          De nature enjouée, joviale et avec un fort esprit d'équipe , je sais m'intégrer rapidement au sein d'une nouvelle équipe ou d'un nouvel environnement de travail. 
+          Motivé et persévérant j'apprends vite et je suis capable de m'adapter rapidement à toutes les situations. Force de proposition et capable d'initiative, je saurai être un atout pour l'équipe.
+          <br /><br />
+          Je me tiens à votre disposition si vous désirez obtenir des informations supplémentaires et je serais ravi de pouvoir m'entretenir avec vous au sujet de votre entreprise et de vos projets.
+          <br /><br /><br />
+          Cordialement. 
+
+        </Modal.Body>
         <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-          Save Changes
+            Fermer
           </Button>
         </Modal.Footer>
       </Modal>
